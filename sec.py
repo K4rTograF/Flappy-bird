bf3b491b25ca8bcd7302ec34888101f4aaf0db6b
K4rTograF <1prostoaleksandr1@gmail.com>
2024-01-23 16:49:37 +0300

    add new

--- sec.py
+++ sec.py
@@ -3,6 +3,8 @@ from PySide6.QtCore import Qt, QTimer
 from PySide6.QtGui import QPixmap
 from PySide6.QtWidgets import QApplication, QGraphicsScene, QGraphicsView, QGraphicsPixmapItem, QGraphicsRectItem, QGraphicsProxyWidget, QPushButton
 
+# for guthub 
+
 class Bird(QGraphicsPixmapItem):
     def __init__(self):  
         super().__init__(QPixmap("bird.png").scaled(50, 50))
